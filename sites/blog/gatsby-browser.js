4b2f562c8860908ea5ede6704907eff72a7c3cd9
signalwerk <sh@signalwerk.ch>
2018-12-01 18:41:43 +0100

    MOD: update postcss

--- sites/blog/gatsby-browser.js
+++ sites/blog/gatsby-browser.js
@@ -1,1 +1,3 @@
-// require("prismjs/themes/prism-okaidia.css")
+require("prismjs/themes/prism-okaidia.css")
+require("slick-carousel/slick/slick.css")
+require("slick-carousel/slick/slick-theme.css")
