cc19b3800f0706a6f8f2543755cc2cbd264d9d55
signalwerk <sh@signalwerk.ch>
2018-12-20 14:53:12 +0000

    FIX: delete dir

--- travis/gh-pages.sh
+++ travis/gh-pages.sh
@@ -49,8 +49,10 @@ echo "   * clean up"
 cd $DEPLOY_DIR
 # Recursively clean current directory but not dir named .git
 echo "   * rm"
-ls -a | grep -v '^\.\.$' | grep -v '^\.$' | grep -v '^\.git$'
-rm -r $(ls -a | grep -v '^\.\.$' | grep -v '^\.$' | grep -v '^\.git$')
+#ls -a | grep -v '^\.\.$' | grep -v '^\.$' | grep -v '^\.git$'
+find . -maxdepth 1 -mindepth 1 -not -name .git 
+# rm -r $(ls -a | grep -v '^\.\.$' | grep -v '^\.$' | grep -v '^\.git$')
+find . -maxdepth 1 -mindepth 1 -not -name .git -exec rm -rf {} \;
 # find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 cd $ROOT_DIR
 
